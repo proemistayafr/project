afbbce66a1cf9a88e7578dc4a449ae166b74e8f5
proemistayafr <nargizanurmukhan@gmail.com>
2023-07-26 00:47:10 +0600

    Third change

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
     <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2 STEP</time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Try our main feature out - click "Get Started" button</h3>
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Try our main feature out - click Get Started button</h3>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">Once you click it, AI will generate information and interesting facts all based on you location.</p>
     </li>&apos;
     <li className="ml-4">
