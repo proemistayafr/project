653bfdd73ff678e1df0f9c99175b1683b65504ff
proemistayafr <nargizanurmukhan@gmail.com>
2023-07-31 10:45:56 +0600

    si

---
+++ app/moblayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './mob.module.css'
+
+const Layout = ({ children }) => {
+
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- app/utils/device.js
+++ app/utils/device.js
@@ -1,4 +1,3 @@
 export function isMobileDevice(userAgent) {
     return /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
   }
-  
