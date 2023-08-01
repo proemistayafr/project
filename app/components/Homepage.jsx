3530319d984b645c7c33f9edc0b8dd7dec0fae93
proemistayafr <nargizanurmukhan@gmail.com>
2023-08-01 13:26:53 +0600

    yes

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -17,7 +17,7 @@ const Homepage = () => {
         </h1> */}
 
 
-          <div className='text-white font-bold py-8 px-4 rounded text-xl md:text-2xl' style={{ fontFamily: 'var(--font-permanent-marker)' }}>
+          <div className='text-white font-bold py-8 px-4 rounded text-xl md:text-2xl mb-8' style={{ fontFamily: 'var(--font-permanent-marker)' }}>
           <p>ZERTTE<span className='text-[#38B000]'>SH</span></p>
           <p className='mt-[-11px] h-[13px] w-350 text-base font-[400]'>VISIT & EXPLORE</p>
         </div>
@@ -26,7 +26,7 @@ const Homepage = () => {
           AND DISCOVER
           ITS HISTORY IN REAL-TIME</h1>
 
-<a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 rounded-[78px] bg-[#84D1FD]">
+<a href="#" className="mb-10 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 rounded-[78px] bg-[#84D1FD]">
     Introducing Zerttesh
     <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- app/components/NewWay.jsx
+++ app/components/NewWay.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const NewWay = () => {
   return (
     <div>     
-<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center mt-8">Experience<span class="text-blue-600 dark:text-blue-500"> A New Way</span> of Researching.</h1>
-<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center"> Our AI-powered web app brings you information and captivating facts
+<h1 class="mb-4 text-4xl font-[400] leading-none tracking-tight  md:text-5xl lg:text-6xl text-center mt-8 text-white"
+    style={{ fontFamily: 'var(--font-permanent-marker)' }}>Experience<span class="text-blue-600 dark:text-blue-500"> A New Way</span> of Researching.</h1>
+<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center mb-16"> Our AI-powered web app brings you information and captivating facts
          about Kazakhstan sights you are visiting. It’s time for a journey of
          discovery, right at your fingertips!.</p>
     </div>
 
   )
-}
-
+  }
 export default NewWay;
