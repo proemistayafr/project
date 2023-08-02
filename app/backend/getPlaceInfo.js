4904831e58f396081d05f0625dc706bda5f263e0
proemistayafr <nargizanurmukhan@gmail.com>
2023-08-03 03:59:11 +0600

    si

--- app/backend/getPlaceInfo.js
+++ app/backend/getPlaceInfo.js
@@ -7,7 +7,7 @@ const getPlaceInfo = async (latitude, longitude) => {
     const response = await axios.get(`${BASE_URL}/get_place_info/${latitude}/${longitude}`);
     return response.data;
   } catch (error) {
-    console.error('Could not find anything about this place, please try again later:', error);
+    console.error('Could not find anything about this place, please try again later', error);
     return null;
   }
 };
