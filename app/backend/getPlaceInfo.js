939136af13bcf589b4bc2ede64a20c45af281cb8
proemistayafr <nargizanurmukhan@gmail.com>
2023-08-04 17:34:51 +0600

    good job

--- app/backend/getPlaceInfo.js
+++ app/backend/getPlaceInfo.js
@@ -4,8 +4,8 @@ const BASE_URL = 'https://zerttesh.onrender.com';
 
 const getPlaceInfo = async (latitude, longitude) => {
   try {
-    const response = await axios.get(`${BASE_URL}/get_info_response/${latitude}/${longitude}`);
-    return response.content;
+    const response = await axios.get(`${BASE_URL}/get_place_info/${latitude}/${longitude}`);
+    return response.data;
   } catch (error) {
     console.error('Could not find anything about this place, please try again later', error);
     return null;
